5c1f8afdb402cbb142dc70a01a15cf8d4a9419fd
Ezra.McNichols <Ezra.McNichols@eimprovement.com>
2018-09-17 11:12:34 -0500

    Added UI.

---
+++ AdinaCardGame/AdinaCardGameUi/ImageCreationProcess.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdinaCardGame
+{
+    public class ImageCreationProcess
+    {
+        private static bool UseOverlay => bool.Parse(ConfigurationManager.AppSettings["OverlayTemplate"]);
+
+        public string Run()
+        {
+            var imageCreator = new ImageCreator();
+            var promptCards = LoadPromptCards();
+            var answerCards = LoadAnswerCards();
+
+            var promptCardFrontImages = promptCards
+                .Select(
+                    (promptCard, index) => new ImageToSave
+                    {
+                        Image = imageCreator.CreatePromptCardFront(promptCard),
+                        Name = $"Prompt Card {index}"
+                    })
+                .ToList();
+            var answerCardFrontImages = answerCards
+                .Select(
+                    (answerCard, index) => new ImageToSave
+                    {
+                        Image = imageCreator.CreateAnswerCardFront(answerCard),
+                        Name = $"Answer Card {index}"
+                    })
+                .ToList();
+
+            var allImages = promptCardFrontImages
+                .Concat(answerCardFrontImages)
+                .ToList();
+
+            if (UseOverlay)
+            {
+                var overlay = new Bitmap(ConfigurationManager.AppSettings["TemplatePath"]);
+                overlay.SetResolution(300, 300);
+                var matrix = new ColorMatrix { Matrix33 = .1f };
+                var attributes = new ImageAttributes();
+                attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+                foreach (var image in allImages)
+                {
+                    var graphics = Graphics.FromImage(image.Image);
+                    graphics.DrawImage(overlay, new Rectangle(0, 0, overlay.Width, overlay.Height), 0, 0, overlay.Width, overlay.Height,
+                        GraphicsUnit.Pixel, attributes);
+
+                }
+            }
+            var outputPath = ConfigurationManager.AppSettings["OutputPath"];
+            var timeStampedFolder = Path.Combine(
+                outputPath,
+                "Images " + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss", CultureInfo.InvariantCulture));
+            Directory.CreateDirectory(timeStampedFolder);
+            foreach (var image in allImages)
+                image.Image.Save($"{timeStampedFolder}\\{image.Name}.png", ImageFormat.Png);
+            return timeStampedFolder;
+        }
+
+        private static IEnumerable<string> LoadAnswerCards()
+        {
+            return File.ReadAllLines(AnswerCardPath)
+                .Select(line => line.Trim())
+                .Where(line => !line.StartsWith("//") && !string.IsNullOrWhiteSpace(line))
+                .ToList();
+        }
+
+        public static string AnswerCardPath => ConfigurationManager.AppSettings["AnswerCardPath"];
+
+        private IEnumerable<string> LoadPromptCards()
+        {
+            return File.ReadAllLines(PromptCardPath)
+                .Select(line => line.Trim())
+                .Where(line => !line.StartsWith("//") && !string.IsNullOrWhiteSpace(line))
+                .ToList();
+        }
+
+        public static string PromptCardPath => ConfigurationManager.AppSettings["PromptCardPath"];
+    }
+}
---
+++ AdinaCardGame/AdinaCardGameUi/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AdinaCardGame;
+
+namespace AdinaCardGameUi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            PromptPathLabel.Content = "Prompt file: " + ImageCreationProcess.PromptCardPath;
+            AnswerPathLabel.Content = "Answer file: " + ImageCreationProcess.AnswerCardPath;
+            base.OnInitialized(e);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(ImageCreationProcess.PromptCardPath))
+            {
+                MessageBox.Show(
+                    $"The prompt file is not available at {ImageCreationProcess.PromptCardPath}. Please make sure this file is available there, or change the configuration file to the correct path.",
+                    "Prompt File Missing",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return;
+            }
+
+            if (!File.Exists(ImageCreationProcess.AnswerCardPath))
+            {
+                MessageBox.Show(
+                    $"The answer file is not available at {ImageCreationProcess.AnswerCardPath}. Please make sure this file is available there, or change the configuration file to the correct path.",
+                    "Prompt File Missing",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return;
+            }
+
+
+            try
+            {
+                var imageCreationProcess = new ImageCreationProcess();
+                var timeStampedFolder = imageCreationProcess.Run();
+                MessageBox.Show($"Images have been created at : {timeStampedFolder}");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Error encountered. Please send Ezra the following:\n{exception}");
+            }
+        }
+    }
+}
